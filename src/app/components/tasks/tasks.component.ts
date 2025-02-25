1b4146fa67e6a889667a53205939dfff24da1e3b
Zeyad Mohamed <zeyadmohamedali@gmail.com>
2025-02-26 01:50:25 +0200

    section 2

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import {NewTaskComponent} from './new-task/new-task.component';
+import {NewTaskData} from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({required: true}) name!: string;
   @Input({required: true}) userId!:string;
-
+  isAddingTask = false;
   tasks = [
     {
       id: 't1',
@@ -39,5 +41,27 @@ export class TasksComponent {
 
   get selectedUserTasks(){
     return this.tasks.filter((task) => task.userId === this.userId)
+  }
+
+  onCompleteTask(id:string){
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+
+  }
+  onCancelAddTask(){
+    this.isAddingTask = false;
+  }
+  onAddTask(taskDate: NewTaskData){
+    this.tasks.unshift({
+      id: new Date().getTime.toString(),
+      userId: this.userId,
+      title: taskDate.title,
+      summary: taskDate.summary,
+      dueDate: taskDate.date
+    })
+    this.isAddingTask = false;
   }
 }
